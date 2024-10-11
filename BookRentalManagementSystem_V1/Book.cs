209ca5b15729eda2bbbbed02a88fb3bb99509598
Thiyagaraja K. <UT01188@unicomtic.com>
2024-10-11 13:55:51 +0530

    QUE_2

---
+++ BookRentalManagementSystem_V1/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRentalManagementSystem_V1
+{
+    internal class Book
+    {
+        private string bookId { get; set; }
+        private string title { get; set; }
+        private string author { get; set; }
+        private decimal rentalPrice { get; set; }
+
+       
+    }
+}
